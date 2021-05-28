6bd1f6e321f8b4fa43f29422159b3d63068c0335
SkillRary <46976867+skillrary@users.noreply.github.com>
2021-05-28 14:29:25 +0530

    Update user-data-to-create-index-html-with-names.sh

--- Elastic Load Balancing and Auto Scaling/user-data-to-create-index-html-with-names.sh
+++ Elastic Load Balancing and Auto Scaling/user-data-to-create-index-html-with-names.sh
@@ -4,7 +4,7 @@ yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
 cd /var/www/html
-aws s3 cp s3://dct-labs-files-elb/names.csv ./
-aws s3 cp s3://dct-labs-files-elb/index.txt ./
+aws s3 cp s3://skillrary-lab-bapei-elb/names.csv ./
+aws s3 cp s3://skillrary-lab-bapei-elb/index.txt ./
 EC2NAME=`cat ./names.csv|sort -R|head -n 1|xargs`
 sed "s/INSTANCEID/$EC2NAME/" index.txt > index.html
